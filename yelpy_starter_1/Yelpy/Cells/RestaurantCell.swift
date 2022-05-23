87835f97e1b1b384e1e5bd62700e1c319e3d8ad0
quangcito <qnguyen2@macalester.edu>
2022-05-23 18:37:48 +0700

    add yelpy

---
+++ yelpy_starter_1/Yelpy/Cells/RestaurantCell.swift
@@ -0,0 +1,23 @@
+//
+//  RestaurantCell.swift
+//  Yelpy
+//
+//  Created by Nguyễn Đương Quang on 5/23/22.
+//  Copyright © 2022 memo. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
---
+++ yelpy_starter_1/Yelpy/Controllers/RestaurantsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Yelpy
+//
+//  Created by Memo on 5/21/20.
+//  Copyright © 2020 memo. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class RestaurantsViewController: UIViewController {
+    
+    // ––––– TODO: Add storyboard Items (i.e. tableView + Cell + configurations for Cell + cell outlets)
+    // ––––– TODO: Next, place TableView outlet here
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // –––––– TODO: Initialize restaurantsArray
+    
+    
+    
+    // ––––– TODO: Add tableView datasource + delegate
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+    
+    
+    // ––––– TODO: Get data from API helper and retrieve restaurants
+    
+
+}
+
+// ––––– TODO: Create tableView Extension and TableView Functionality
+
+
